67c6042370c2b5281601310425f9bb32ddbdb420
Christopher Carson <chris@nowzoo.com>
2022-09-15 11:52:09 -0500

    show typescript error

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,14 @@
+import { invalid, type Actions, type ActionResult } from '@sveltejs/kit';
+
+type Invalid = { message: string; lucky: number }
+type Success = { message: string; lucky: number };
+
+export const actions: Actions = {
+	default: (): ActionResult<Success, Invalid> => {
+		const lucky = Math.random();
+		if (lucky > 0.499999) {
+			return { status: 200, type: 'success', data: { message: 'Yay!', lucky } };
+		}
+		return invalid(400, { message: 'Boo', lucky });
+	}
+};
